ee3b9a171b8ed821dbf472f3482923d0317aa312
creek0810 <creek880810@gmail.com>
2019-11-22 15:24:29 +0800

    feat: finish logout
    url:
      - /user/logout

--- views/user_web.py
+++ views/user_web.py
@@ -1,11 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, redirect, url_for
+
+from flask_login import logout_user
 
 user_web = Blueprint('user_web', __name__)
 
 
 @user_web.route('/logout', methods=["GET"])
 def logout():
-    return "logout"
+    logout_user()
+    return redirect(url_for('menu_web.menu'))
 
 
 @user_web.route('/password/reset', methods=["GET"])
