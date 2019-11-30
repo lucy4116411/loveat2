640e10792b1852cb4319d6d98aa5f090707349d6
creek0810 <creek880810@gmail.com>
2019-11-30 17:14:43 +0800

    feat: finish add, update type api
    url:
      - /api/menu/type/update
      - /api/menu/type/new

--- models/menu.py
+++ models/menu.py
@@ -2,7 +2,10 @@ from bson.objectid import ObjectId
 
 from config import URL
 
+from lib.custom_except import duplicateError
+
 from pymongo import MongoClient
+
 
 DB = MongoClient(URL)["loveat2"]
 TYPE_COLLECTION = DB["type"]
@@ -100,3 +103,21 @@ def delete_combo(id):
     combo = COMBO_COLLECTION.find_one({"_id": object_id}, {"picture": 1})
     COMBO_COLLECTION.delete_one({"_id": ObjectId(id)})
     IMAGE_COLLECTION.delete_one({"uuid": combo["picture"]})
+
+
+def add_type(data):
+    cur_type = TYPE_COLLECTION.find_one(
+        {"category": data["category"], "name": data["type"]}
+    )
+    if cur_type:
+        raise duplicateError
+    else:
+        TYPE_COLLECTION.insert_one(
+            {"category": data["category"], "name": data["type"]}
+        )
+
+
+def update_type(data):
+    TYPE_COLLECTION.update_one(
+        {"_id": ObjectId(data["id"])}, {"$set": {"name": data["type"]}}
+    )
--- views/menu_api.py
+++ views/menu_api.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request
 
 from lib.auth import admin_required
+from lib.custom_except import duplicateError
 
 from models import menu
 
@@ -11,6 +12,25 @@ menu_api = Blueprint("menu_api", __name__)
 @menu_api.route("/", methods=["GET"])
 def get_menu():
     return jsonify(menu.get_all())
+
+
+@menu_api.route("/type/new", methods=["POST"])
+@admin_required
+def add_type():
+    data = request.get_json()
+    try:
+        menu.add_type(data)
+        return "", 200
+    except duplicateError:
+        return "", 409
+
+
+@menu_api.route("/type/update", methods=["POST"])
+@admin_required
+def update_type():
+    data = request.get_json()
+    menu.update_type(data)
+    return "", 200
 
 
 @menu_api.route("/item", methods=["POST"])
