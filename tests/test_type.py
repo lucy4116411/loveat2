3a8d17eae7e6ba99f175efcbb6f53e3c883a8668
creek0810 <creek880810@gmail.com>
2019-12-02 21:05:14 +0800

    feat: add update type test

--- tests/test_type.py
+++ tests/test_type.py
@@ -4,6 +4,7 @@ URL_PREFIX = "/api/menu/type"
 
 
 class TestType(object):
+    # add operation
     def test_add_success(self, client, admin):
         # test if there is no muffin burger in type collection
         new_type = client.application.config["db"]["TYPE_COLLECTION"].find_one(
@@ -68,7 +69,7 @@ class TestType(object):
         # test if there is only one muffin burger in type collection
         new_type = list(
             client.application.config["db"]["TYPE_COLLECTION"].find(
-                {"name": "muffin burger"}
+                {"name": "testType"}
             )
         )
         assert len(new_type) == 1
@@ -76,14 +77,105 @@ class TestType(object):
         url = URL_PREFIX + "/new"
         rv = client.post(
             url,
-            data=json.dumps({"category": "item", "type": "muffin burger"}),
+            data=json.dumps({"category": "item", "type": "testType"}),
             content_type="application/json",
         )
         assert rv.status_code == 409
         # test if there is only one muffin burger in type collection
         new_type = list(
             client.application.config["db"]["TYPE_COLLECTION"].find(
-                {"name": "muffin burger"}
+                {"name": "testType"}
             )
         )
         assert len(new_type) == 1
+
+    # update operation
+    def test_update_success(self, client, admin):
+        # test if testType exists and newTestType not exists
+        old_type = client.application.config["db"]["TYPE_COLLECTION"].find_one(
+            {"name": "testType"}
+        )
+        assert old_type is not None
+        new_type = client.application.config["db"]["TYPE_COLLECTION"].find_one(
+            {"name": "newTestType"}
+        )
+        assert new_type is None
+        # test update type api
+        url = URL_PREFIX + "/update"
+        rv = client.post(
+            url,
+            data=json.dumps(
+                {"id": str(old_type["_id"]), "type": "newTestType"}
+            ),
+            content_type="application/json",
+        )
+        assert rv.status_code == 200
+        # test if testType not exists and newTestType exists
+        old_type = client.application.config["db"]["TYPE_COLLECTION"].find_one(
+            {"name": "testType"}
+        )
+        assert old_type is None
+        new_type = client.application.config["db"]["TYPE_COLLECTION"].find_one(
+            {"name": "newTestType"}
+        )
+        assert new_type is not None
+
+    def test_update_unauthorized(self, client):
+        # test if testType exists and newTestType not exists
+        old_type = client.application.config["db"]["TYPE_COLLECTION"].find_one(
+            {"name": "testType"}
+        )
+        assert old_type is not None
+        new_type = client.application.config["db"]["TYPE_COLLECTION"].find_one(
+            {"name": "newTestType"}
+        )
+        assert new_type is None
+        # test update type api
+        url = URL_PREFIX + "/update"
+        rv = client.post(
+            url,
+            data=json.dumps(
+                {"id": str(old_type["_id"]), "type": "newTestType"}
+            ),
+            content_type="application/json",
+        )
+        assert rv.status_code == 403
+        # test if testType exists and newTestType not exists
+        old_type = client.application.config["db"]["TYPE_COLLECTION"].find_one(
+            {"name": "testType"}
+        )
+        assert old_type is not None
+        new_type = client.application.config["db"]["TYPE_COLLECTION"].find_one(
+            {"name": "newTestType"}
+        )
+        assert new_type is None
+
+    def test_update_by_customer(self, client, customer):
+        # test if testType exists and newTestType not exists
+        old_type = client.application.config["db"]["TYPE_COLLECTION"].find_one(
+            {"name": "testType"}
+        )
+        assert old_type is not None
+        new_type = client.application.config["db"]["TYPE_COLLECTION"].find_one(
+            {"name": "newTestType"}
+        )
+        assert new_type is None
+        # test update type api
+        url = URL_PREFIX + "/update"
+        rv = client.post(
+            url,
+            data=json.dumps(
+                {"id": str(old_type["_id"]), "type": "newTestType"}
+            ),
+            content_type="application/json",
+        )
+        assert rv.status_code == 403
+        # test if testType exists and newTestType not exists
+        old_type = client.application.config["db"]["TYPE_COLLECTION"].find_one(
+            {"name": "testType"}
+        )
+        assert old_type is not None
+        new_type = client.application.config["db"]["TYPE_COLLECTION"].find_one(
+            {"name": "newTestType"}
+        )
+        assert new_type is None
