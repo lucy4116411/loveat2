f3e99ccfdd1d179d313bf019b801b81cec8bc012
huang <linda0981347815@gmail.com>
2019-12-03 13:49:39 +0800

    feat(business_time): add the ability of setting business time

--- views/setting_web.py
+++ views/setting_web.py
@@ -4,12 +4,19 @@ from flask_login import current_user
 
 from lib.auth import admin_required
 
+import models.business_time
+
 setting_web = Blueprint("setting_web", __name__)
 
 
-@setting_web.route("/business-time", methods=["GET"])
+@setting_web.route("/business-time/", methods=["GET"])
 @admin_required
 def business_time():
+    everyday_work_time = models.business_time.get()
+    # print(everyday_work_time)
     return render_template(
-        "business_time.html", auth=current_user.role, name=current_user.name
+        "business_time.html",
+        work_time=everyday_work_time,
+        auth=current_user.role,
+        name=current_user.name,
     )
