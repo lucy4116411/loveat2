b751d6209ab084b7cd1d0aa96955f21f0bbcc339
PeggyLi <suara1201fxt@gmail.com>
2019-12-04 02:43:34 +0800

    feat: finish cart.js and cart page

--- static/js/lib/cart.js
+++ static/js/lib/cart.js
@@ -1,38 +1,97 @@
-// eslint-disable-next-line no-unused-vars
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-unused-vars */
+/* global FetchData */
+
+/**
+ * @description do operating at local storage
+ *
+ * @class Cart
+ */
 class Cart {
   constructor() {
-    this.prefix = 'loveat2-cart';
-    if (this.prefix in localStorage) {
-      this.content = JSON.parse(localStorage.getItem(this.prefix));
-    } else {
-      this.content = {};
-    }
+    return (async () => {
+      this.prefix = 'loveat2-cart';
+      if (this.prefix in localStorage) {
+        this.content = JSON.parse(localStorage.getItem(this.prefix));
+        const itemList = [];
+        const comboList = [];
+        Object.keys(this.content).forEach((key) => {
+          if (this.content[key].category === 'item') {
+            itemList.push(key);
+          } else {
+            comboList.push(key);
+          }
+        });
+        const itemResult = await FetchData.post('/api/menu/item', itemList).then((res) => res.json());
+        const comboResult = await FetchData.post('/api/menu/combo', comboList).then((res) => res.json());
+        itemResult.forEach((cur) => {
+          this.content[cur._id].price = cur.price;
+          this.content[cur._id].name = cur.name;
+        });
+        comboResult.forEach((cur) => {
+          this.content[cur._id].price = cur.price;
+          this.content[cur._id].name = cur.name;
+        });
+      } else {
+        this.content = {};
+      }
+      return this;
+    })();
   }
 
+  /**
+   * @description update the order which key is loveat2-cart
+   *
+   * @memberof Cart
+   */
   updateLocalStorage() {
     localStorage.setItem(this.prefix, JSON.stringify(this.content));
   }
 
+  /**
+   * @description add the item quantity which key is loveat2-cart
+   *
+   * @param {*} data
+   * @memberof Cart
+   */
   add(data) {
-    if (data.id in this.content) {
-      this.content[data.id].quantity += 1;
+    if (data._id in this.content) {
+      this.content[data._id].quantity += 1;
     } else {
-      this.content[data.id] = {
+      this.content[data._id] = {
+        category: data.category,
         quantity: data.quantity,
       };
     }
     this.updateLocalStorage();
   }
 
+  /**
+   * @description get order form local storage
+   *
+   * @returns
+   * @memberof Cart
+   */
   get() {
     return this.content;
   }
 
+  /**
+   * @description delete the target item by key
+   *
+   * @param {*} key index of deleting item
+   * @memberof Cart
+   */
   delete(key) {
     delete this.content[key];
     this.updateLocalStorage();
   }
 
+  /**
+   * @description remove the item which key is loveat2-cart
+   *
+   * @memberof Cart
+   */
   clear() {
     localStorage.removeItem(this.prefix);
   }
--- static/js/loveat2-cart.js
+++ static/js/loveat2-cart.js
@@ -1,99 +1,9 @@
-/* eslint-disable max-len */
-/* eslint-disable guard-for-in */
-/* eslint-disable no-underscore-dangle */
-/* eslint-disable no-restricted-syntax */
-/* golbal FetchData, Cart */
+/* eslint-disable no-unused-vars */
+/* global Cart, $, FetchData */
+/* eslint no-underscore-dangle: ["error", {"allow":["_id"]}] */
+let myCart = null;
 
-const TestCase = [
-  {
-    _id: '5dd67f098f0f6afb3ebc1b6b',
-    category: 'item',
-    quantity: 1,
-  },
-  {
-    _id: '5dda567d09d84aa89699121c',
-    category: 'combo',
-    quantity: 1,
-  },
-  {
-    _id: '5dd67f098f0f6afb3ebc1b6a',
-    category: 'item',
-    quantity: 1,
-  },
-  {
-    _id: '5dd67f098f0f6afb3ebc1b69',
-    category: 'item',
-    quantity: 1,
-  },
-  {
-    _id: '5dda57c047b0e46ffecb2b9a',
-    category: 'combo',
-    quantity: 1,
-  },
-];
-
-
-const PREFIX = 'loveat2-cart';
-
-
-function clearItem() {
-  localStorage.clear();
-}
-
-async function addItem(dataSet) {
-  const tempOrder = await dataSet;
-  const jsonOrder = JSON.stringify(tempOrder);
-  localStorage.setItem(PREFIX, jsonOrder);
-}
-
-async function assignData(item, combo) {
-  const tmp = [];
-  const itemTmp = await item;
-  const comboTmp = await combo;
-
-  for (let i = 0; i < itemTmp.length; i += 1) {
-    itemTmp[i].category = 'item';
-    itemTmp[i].quantity = 1;
-    tmp.push(itemTmp[i]);
-  }
-  for (let j = 0; j < comboTmp.length; j += 1) {
-    comboTmp[j].category = 'combo';
-    comboTmp[j].quantity = 1;
-    tmp.push(comboTmp[j]);
-  }
-  return tmp;
-}
-
-
-async function getItem() {
-  const dataSet = JSON.parse(localStorage.getItem(PREFIX));
-  const itemId = [];
-  const comboId = [];
-  for (const data in dataSet) {
-    if (dataSet[data].category === 'item') {
-      itemId.push(dataSet[data]._id);
-    } else if (dataSet[data].category === 'combo') {
-      comboId.push(dataSet[data]._id);
-    }
-  }
-  const itemData = await FetchData.post('/api/menu/item', itemId);
-  const comboData = await FetchData.post('/api/menu/combo', comboId);
-  const tmpItem = itemData.json();
-  const tmpCombo = comboData.json();
-  const tmp = assignData(tmpItem, tmpCombo);
-  addItem(tmp);
-  return tmp;
-}
-
-
-function clearOrder() {
-  document.getElementById('cart-table').innerHTML = '';
-  localStorage.removeItem(PREFIX);
-}
-
-
-async function drawItem(dataPromise) {
-  const data = await dataPromise;
+function drawItem(data) {
   const result = Object.keys(data).reduce((acc, key) => `${acc} <tr id="${key}">
               <td>${data[key].name}</td>
               <td>
@@ -101,7 +11,7 @@ async function drawItem(dataPromise) {
               </td>
               <td>${data[key].price}</td>
               <td>
-                <input type="text" id="description-${key}" onclick="changeQuantity(this.id)">
+                <input type="text" id="description-${key}">
               </td>
               <td id="item-sum-${key}" >${data[key].price}</td>
               <td>
@@ -112,91 +22,100 @@ async function drawItem(dataPromise) {
 }
 
 
-function setItem(data) {
-  const itemJsonString = JSON.stringify(data);
-  localStorage.setItem(PREFIX, itemJsonString);
-}
-
-
 function deleteItem(key) {
-  const tar = key.charAt(key.length - 1);
-  const dataSet = JSON.parse(localStorage.getItem(PREFIX));
-  dataSet.splice(tar, 1);
-  // addItem
-  if (Object.keys(dataSet).length === 0) {
-    localStorage.removeItem(PREFIX);
-    // Cart.clear();
-  } else {
-    const jsonOrder = JSON.stringify(dataSet);
-    localStorage.setItem(PREFIX, jsonOrder);
-  }
-  drawItem(getItem());
+  const id = key.substr(7);
+  myCart.delete(id);
+  document.getElementById(id).innerHTML = '';
 }
 
-// 計算單品總價
-async function calculateItemPrice(key, quantity) {
-  const allItem = getItem();
-  const data = await allItem;
-  const tar = key.charAt(key.length - 1);
-  document.getElementById(`item-sum-${tar}`).innerHTML = quantity * data[tar].price;
+function orderTotal() {
+  let total = 0;
+  const dataSet = myCart.get();
+  Object.keys(dataSet).forEach((key) => {
+    total += dataSet[key].quantity * dataSet[key].price;
+  });
+  document.getElementById('total').innerHTML = total;
 }
 
-async function changeQuantity(key) {
+function changeQuantity(key) {
+  const id = key.substr(9);
   const newQuantity = document.getElementById(key).value;
-  const tar = key.charAt(key.length - 1);
-  const dataSet = await getItem();
-  dataSet[tar].quantity = parseInt(newQuantity, 10);
-  for (let i = 0; i < dataSet.length; i += 1) {
-    console.log(dataSet[i]);
-  }
-
-  // addItem
-  const jsonOrder = JSON.stringify(dataSet);
-  console.log(jsonOrder);
-  localStorage.removeItem(PREFIX);
-  localStorage.setItem(PREFIX, jsonOrder);
-  // const result = `<input type="number" id="${key}" min="1" value="${newQuantity}" onclick="changeQuantity(this.id)">`;
-  // document.getElementById(key).innerHTML = '';
-  // document.getElementById(key).innerHTML = result;
-  calculateItemPrice(key, newQuantity);
+  const order = myCart.get();
+  order[id].quantity = newQuantity;
+  myCart.updateLocalStorage();
+  document.getElementById(`item-sum-${id}`).innerHTML = order[id].price * order[id].quantity;
+  orderTotal();
 }
 
-function orderSum() {
-
+function orderDescription() {
+  const description = [];
+  const dataSet = myCart.get();
+  Object.keys(dataSet).forEach((key) => {
+    const text = document.getElementById(`description-${key}`).value;
+    if (text !== '') {
+      description.push(`${dataSet[key].name}:${text}`);
+    }
+  });
+  return description.join(';');
 }
 
-async function dealOrder() {
+function orderContent() {
   const postOrder = [];
-  const dataSet = await getItem();
-  for (const data in dataSet) {
+  const dataSet = myCart.get();
+
+  Object.keys(dataSet).forEach((key) => {
     const result = {};
-    console.log(dataSet[data]);
-    result.id = dataSet[data]._id;
-    result.category = dataSet[data].category;
-    result.quantity = dataSet[data].quantity;
+    result.id = key;
+    result.category = dataSet[key].category;
+    result.quantity = dataSet[key].quantity;
     postOrder.push(result);
-  }
-  console.log(postOrder);
+  });
+
   return postOrder;
 }
 
+function clearOrder() {
+  myCart.clear();
+  document.getElementById('cart-table').innerHTML = '';
+}
+
+function toState() {
+  window.location = '/order/state';
+}
+
+function checkStatus(state) {
+  if (state === 200) {
+    document.getElementById('close-modal').addEventListener('click', toState);
+    document.getElementById('hint-content').innerHTML = '下單成功';
+    $('#order-send-hint-modal').modal('show');
+    clearOrder();
+  } else if (state === 401) {
+    document.getElementById('hint-content').innerHTML = '您尚未登入';
+    $('#order-send-hint-modal').modal('show');
+  } else {
+    document.getElementById('hint-content').innerHTML = '訂單錯誤或預定取餐時間未營業';
+    $('#order-send-hint-modal').modal('show');
+  }
+}
+
 async function sendOrder() {
-  const order = [];
+  const order = {};
+  const total = document.getElementById('total').innerHTML;
   order.takenAt = document.getElementById('order-take-time').value;
-  order.notes = '不要番茄醬';
-  order.total = 490;
-  order.content = await dealOrder();
-  console.log(order);
-  FetchData.post('/api/order/new', order);
+  order.notes = orderDescription();
+  order.total = parseInt(total, 10);
+  order.content = orderContent();
+  const response = await FetchData.post('/api/order/new', order);
+  checkStatus(response.status);
 }
 
 
 async function init() {
-  // add button listener
+  myCart = await new Cart();
+  orderTotal();
+  drawItem(myCart.get());
   document.getElementById('clear-order').addEventListener('click', clearOrder);
   document.getElementById('send-order').addEventListener('click', sendOrder);
-  await addItem(TestCase);
-  drawItem(getItem());
 }
 
 window.addEventListener('load', init);
