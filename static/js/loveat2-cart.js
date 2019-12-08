485a5104c576f899cf754599dde893e8fc16c8bd
creek0810 <creek880810@gmail.com>
2019-12-08 13:47:54 +0800

    fix(cart-page): fix cart type of quantity in local storage
    fix #56

--- static/js/loveat2-cart.js
+++ static/js/loveat2-cart.js
@@ -4,23 +4,6 @@
 
 let myCart = null;
 
-function drawItem(data) {
-  const result = Object.keys(data).reduce((acc, key) => `${acc} <tr id="${key}">
-              <td>${data[key].name}</td>
-              <td>
-                <input type="number" id="quantity-${key}" min="1" value="${data[key].quantity}" onclick="changeQuantity(this.id)">
-              </td>
-              <td>${data[key].price}</td>
-              <td>
-                <input type="text" id="description-${key}">
-              </td>
-              <td id="item-sum-${key}" >${data[key].price * data[key].quantity}</td>
-              <td>
-                <button id="delete-${key}" class="btn btn-primary" type="button" onClick="deleteItem(this)">Delete</button>
-              </td>
-            </tr>`, '');
-  document.querySelector('#cart-table tbody').innerHTML = result;
-}
 
 function orderTotal() {
   let total = 0;
@@ -32,8 +15,8 @@ function orderTotal() {
 }
 
 function deleteItem(e) {
-  const itemId = e.id.substr(7);
-  const key = e.parentNode.parentNode.rowIndex;
+  const itemId = e.target.id.substr(7);
+  const key = e.target.parentNode.parentNode.rowIndex;
   myCart.delete(itemId);
   document.getElementById('cart-table').deleteRow(key);
   orderTotal();
@@ -45,9 +28,9 @@ function updateSum(id) {
   document.getElementById(`item-sum-${id}`).innerHTML = order[id].price * order[id].quantity;
 }
 
-function changeQuantity(key) {
-  const newQuantity = document.getElementById(key).value;
-  const id = key.substr(9);
+function changeQuantity(e) {
+  const newQuantity = parseInt(e.target.value, 10);
+  const id = e.target.id.substr(9);
   myCart.updateQuantity(id, newQuantity);
   updateSum(id);
   orderTotal();
@@ -112,6 +95,35 @@ async function sendOrder() {
   order.content = orderContent();
   const response = await FetchData.post('/api/order/new', order);
   checkStatus(response.status);
+}
+
+function drawItem(data) {
+  const result = Object.keys(data).reduce((acc, key) => `${acc} <tr id="${key}">
+              <td>${data[key].name}</td>
+              <td>
+                <input type="number" id="quantity-${key}" min="1" value="${data[key].quantity}" class="quantity-input form-control">
+              </td>
+              <td>${data[key].price}</td>
+              <td>
+                <input class="form-control" type="text" id="description-${key}">
+              </td>
+              <td id="item-sum-${key}" >${data[key].price * data[key].quantity}</td>
+              <td>
+                <button id="delete-${key}" class="btn btn-primary delete-item" type="button">Delete</button>
+              </td>
+            </tr>`, '');
+  // draw it
+  document.querySelector('#cart-table tbody').innerHTML = result;
+  // add event listener
+  const quantityInput = document.getElementsByClassName('quantity-input');
+  [...quantityInput].forEach((cur) => {
+    cur.addEventListener('change', changeQuantity);
+  });
+
+  const deleteBtn = document.getElementsByClassName('delete-item');
+  [...deleteBtn].forEach((cur) => {
+    cur.addEventListener('click', deleteItem);
+  });
 }
 
 
