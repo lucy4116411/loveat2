3d6a33ea43b61a9aa9e0dcf9a46e8dcb0f70f0c6
creek0810 <creek880810@gmail.com>
2019-12-16 23:40:48 +0800

    test: add test for delete type, update combo api

--- tests/test_type.py
+++ tests/test_type.py
@@ -1,11 +1,21 @@
 import json
 
+from bson.objectid import ObjectId
+
 from models import db
 
 URL_PREFIX = "/api/menu/type"
 
 
 class TestType(object):
+    # 吐司
+    delete_item_type = {"id": "5dd678b95f19051c7c4f0bb3"}
+    # 鐵板麵套餐
+    delete_combo_type = {"id": "5dd681c44a608a104f899151"}
+    # undefined type
+    undefined_item_type = ObjectId("5de2298f558c6ebb84d05c17")
+    undefined_combo_type = ObjectId("5de22978558c6ebb84d05c14")
+
     # add operation
     def test_add_success(self, client, admin):
         # test if there is no muffin burger in type collection
@@ -137,3 +147,80 @@ class TestType(object):
         assert old_type is not None
         new_type = db.TYPE_COLLECTION.find_one({"name": "newTestType"})
         assert new_type is None
+
+    # delete operation
+    def test_delete_unauthorized(self, client):
+        url = URL_PREFIX + "/delete"
+        rv = client.post(
+            url,
+            data=json.dumps(self.delete_item_type),
+            content_type="application/json",
+        )
+        assert rv.status_code == 403
+        cur_type = db.TYPE_COLLECTION.find_one(
+            {"_id": ObjectId(self.delete_item_type["id"])}
+        )
+        assert cur_type is not None
+
+    def test_delete_by_customer(self, client, customer):
+        url = URL_PREFIX + "/delete"
+        rv = client.post(
+            url,
+            data=json.dumps(self.delete_item_type),
+            content_type="application/json",
+        )
+        assert rv.status_code == 403
+        cur_type = db.TYPE_COLLECTION.find_one(
+            {"_id": ObjectId(self.delete_item_type["id"])}
+        )
+        assert cur_type is not None
+
+    def test_delete_item_type_success(self, client, admin):
+        # get original item in "吐司" type
+        items = list(
+            db.ITEM_COLLECTION.find(
+                {"type": ObjectId(self.delete_item_type["id"])}
+            )
+        )
+        # start delete
+        url = URL_PREFIX + "/delete"
+        rv = client.post(
+            url,
+            data=json.dumps(self.delete_item_type),
+            content_type="application/json",
+        )
+        assert rv.status_code == 200
+        # check delete success
+        cur_type = db.TYPE_COLLECTION.find_one(
+            {"_id": ObjectId(self.delete_item_type["id"])}
+        )
+        assert cur_type is None
+        # check the items in "吐司" type update their type to "未分類（單品）"
+        for item in items:
+            tmp = db.ITEM_COLLECTION.find_one({"_id": ObjectId(item["_id"])})
+            assert tmp["type"] == self.undefined_item_type
+
+    def test_delete_item_combo_success(self, client, admin):
+        # get original item in "鐵板麵套餐" type
+        combos = list(
+            db.COMBO_COLLECTION.find(
+                {"type": ObjectId(self.delete_combo_type["id"])}
+            )
+        )
+        # start delete
+        url = URL_PREFIX + "/delete"
+        rv = client.post(
+            url,
+            data=json.dumps(self.delete_combo_type),
+            content_type="application/json",
+        )
+        assert rv.status_code == 200
+        # check delete success
+        cur_type = db.TYPE_COLLECTION.find_one(
+            {"_id": ObjectId(self.delete_combo_type["id"])}
+        )
+        assert cur_type is None
+        # check the items in "鐵板麵套餐" type update their type to "未分類（套餐）"
+        for item in combos:
+            tmp = db.COMBO_COLLECTION.find_one({"_id": ObjectId(item["_id"])})
+            assert tmp["type"] == self.undefined_combo_type
